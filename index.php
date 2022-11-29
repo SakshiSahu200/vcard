77cbed88ff1923e0b6364f0f817335d7931eb5c6
Sakshi_Sahu <sakshisahu.3475@gmail.com>
2022-11-29 19:32:31 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <title>vCard</title>
+</head>
+
+
+
+<body>
+
+    <div class="card">
+        <div class="header">
+           <!-- <img src="./img/mobiledev1.jpg"> -->
+            <h3>Sakshi Sahu</h3>
+            <p style="text-align: center;">Web Developer</p>
+        </div>
+
+        <div class="body">
+            <ul>
+                <li>
+                    <h3>Name</h3>
+                    <p>Sakshi Sahu</p>
+                    <i class='fas fa-user-alt' style='font-size:24px'></i>
+                    <!-- <i class="fas fa-globe-americas"></i> -->
+                </li>
+                <li>
+                    <h3>9165609515</h3>
+                    <p>Mobile number</p>
+                    <i class="fas fa-mobile-alt"></i>
+                </li>
+                <li>
+                    <h3>sakshisahu.3475@gmail.com</h3>
+                    <p>Email address</p>
+                    <i class="fas fa-envelope"></i>
+                </li>
+                
+            </ul>
+        </div>
+
+        <div class="footer">
+            <ul>
+                <li>
+                    <i class="fab fa-facebook-f"></i>
+                </li>
+                <li>
+                    <i class="fab fa-twitter"></i>
+                </li>
+                <li>
+                    <i class="fab fa-instagram"></i>
+                </li>
+                <li>
+                    <i class="fab fa-linkedin-in"></i>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
